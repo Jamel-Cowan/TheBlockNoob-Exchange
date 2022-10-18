f9608533398872f3584e25906218dcf42df705b1
Block Noob <98855710+Jamel-Cowan@users.noreply.github.com>
2022-10-18 11:20:17 -0400

    Create and test Exchange smart contract 001

---
+++ src/contracts/Exchange.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Exchange {
+
+    
+}
+
+
+
+// deposit and withdraw funds
+// Manage orders make or cancel
+// Handle trades - Charge fees
+
+
+
+// TO DO:
+// [ ] Set the fee account
+// [ ] Deposit ether
+// [ ] Withdraw ether
+// [ ] Deposit tokens
+// [ ] Withdraw tokens
+// [ ] Check balances
+// [ ] Make order
+// [ ] Cancel order
+// [ ] Fill order
+// [ ] Charge fees
